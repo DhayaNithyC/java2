258d4835cfe724001928cd26a3f47555f7c01f90
DhayaNithyC <49777415+DhayaNithyC@users.noreply.github.com>
2021-04-09 22:11:33 +0530

    Create codequality.cmd

---
+++ codequality.cmd
@@ -0,0 +1,8 @@
+@echo off
+
+set CHECKSTYLE_LIB=..\iText\Application\lib\checkstyle
+set CLASSPATH=%CLASSPATH%;%CHECKSTYLE_LIB%\checkstyle-5.5.jar
+set CLASSPATH=%CLASSPATH%;%CHECKSTYLE_LIB%\google-collections-1.0.jar
+set CLASSPATH=%CLASSPATH%;%CHECKSTYLE_LIB%\commons-beanutils-core-1.8.3.jar
+set CLASSPATH=%CLASSPATH%;%CHECKSTYLE_LIB%\antlr-2.7.7.jar
+set CLASSPATH=%CLASSPATH%;%CHECKSTYLE_LIB%\commons-logging.jar
